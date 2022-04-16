fd344939faabfb9e82d3d166507bfa614244b6a7
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-04-17 01:07:28 +0600

    some necessary package downloaded

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h2>Photographer</h2>
+            </div>
+            <ul>
+                <li>home</li>
+                <li>service</li>
+                <li>login</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
