f1d1e4f176cd3b6b3393775157efabf51c667568
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-04-17 13:37:40 +0600

    file structure done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './pages/Header/Header';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
