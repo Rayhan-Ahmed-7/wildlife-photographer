d3dde3abeec59560e097c700e4bf109607ae52dd
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-04-18 02:52:48 +0600

    not found page added

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import not from '../../images/notfound/404.png';
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto d-flex flex-lg-row flex-column align-items-center mt-4'>
+            <div>
+                <small className='text-uppercase letter-spacing text-gray'>error 404!</small>
+                <h2 className='text-uppercase fw-bold text-gray'>The page you looking for is not found.!</h2>
+            </div>
+            <div>
+                <img className='img-fluid' src={not} alt="" />
+            </div>
         </div>
     );
 };
