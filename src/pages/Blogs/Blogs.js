be6fcccc49924ea32369b5324575371db4a0a07b
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-04-18 13:18:24 +0600

    blogs page done

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -5,15 +5,26 @@ const Blogs = () => {
         <div className='container mx-auto row row-cols-lg-2 row-cols-1 pt-4'>
             <div>
                 <h2 className='text-dark fs-4'>1. what is the Difference between authorization and authentication .?</h2>
-                <p></p>
+                <p className='row row-cols-2'>
+                    <span className='fs-5 d-block text-center'>Authentication</span>
+                    <span className='fs-5 d-block text-center'>Authorization</span>
+                    <span className='text-justify mb-2 fw-normal'>1. Determines whether users are valid or not or who they claim to be in short identifying the user.</span>
+                    <span className='text-justify mb-2 fw-normal'>1. Determines what resources users can access or not.</span>
+                    <span className='text-justify mb-2 fw-normal'>2. Usually done before authorization.</span>
+                    <span className='text-justify mb-2 fw-normal'>2. Usually done after successful authentication.</span>
+                    <span className='text-justify mb-2 fw-normal'>3. An example: Employees in a company are required to authenticate through the network before accessing their company email.</span>
+                    <span className='text-justify mb-2 fw-normal'>3. An example: After an employee successfully authenticates, the system determines what information the employees are allowed to access.</span>
+                </p>
             </div>
             <div>
                 <h2 className='text-dark fs-4'>2. Why are you using firebase.? What other options do you have to implement authentication .?</h2>
-                <p></p>
+                <p className='text-justify'>1.Firebase Authentication makes it easier to get your users signed-in without having to understand the complexities behind implementing our own authentication system.</p>
+                <p className='text-justify'>2.Other option: AWS Amplify is also a open-source backend as a service in cloud computing since it’s created by Amazon. This comes with a full toolkit that can be expected to handle multiple backend services in a more manageable and simpler way.</p>
+                <p className='text-justify'>2.1 Back4App is also one of the easiest solutions that are available on the market that is designed for the creation of web and mobile apps. It also allows you to host and manage your applications with ease.</p>
             </div>
             <div>
                 <h2 className='text-dark fs-4'>3. What other services does firebase provide other than authentication .?</h2>
-                <p></p>
+                <p className='text-justify'>Firebase provide a lot of services other than authentication like Hosting, cloud firestore, cloud storage, Google Analytics, predictions, cloud messaging etc</p>
             </div>
         </div>
     );
--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CheckOut = () => {
     return (
-        <div>
+        <div className='container mx-auto pt-4 min-vh-100'>
             <h2>Check Out</h2>
         </div>
     );
